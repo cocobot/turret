eb81ca9a9a3a62d2e63458ed51b9c74f7d1f99ec
Damien Espitallier <damien.espitallier@gmail.com>
2015-07-04 17:43:37 +0200

    base project with ausbee_avr/freertos

--- main.c
+++ main.c
@@ -1,16 +1,37 @@
+#include <FreeRTOS.h>
+#include <task.h>
 #include <platform.h>
 #include <mcual.h>
+
+void s(char * str)
+{
+  while(*str)
+  {
+    mcual_usart_send(PLATFORM_USART_BAL1, *str);
+    str += 1;
+  }
+}
+
+void blink(void * arg)
+{
+  (void)arg;
+  int i = 0;
+  while(1)
+  {
+    platform_led_toggle(1 << (i % 6));
+    i += 1;
+
+    vTaskDelay(75 / portTICK_RATE_MS);
+  }
+}
 
 int main(void)
 {
   platform_init();
 
-  while(1) 
-  {
-    platform_led_toggle(0xffffffff);
-    volatile uint32_t tempo;
-    for(tempo = 0; tempo < 10000; tempo += 1);
-  }
+  xTaskCreate(blink, "blink", 400, NULL, 1, NULL );
+
+  vTaskStartScheduler();
 
   return 0;
 }
